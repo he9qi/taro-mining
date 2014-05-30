a00b6f3b42935d0a536459ab61bb75a295427725
Qi He <qhe@heyook.com>
2014-05-30 14:05:01 +0800

    read all files from layered dirs

--- tests/test_taro_data.R
+++ tests/test_taro_data.R
@@ -12,21 +12,21 @@ context("  read data from directory that doesn't have the files")
 
 # TaroData.readDataFromDir
 test_that("  read data from directory that doesn't have the files", {
-  data <- TaroData.readDataFromDir(test.data_path, pattern=".r")
+  data <- TaroData.readDataFromDir(test.data_path, pattern="unkown")
   # should return NULL
   expect_equivalent(data, NULL) 
 })
 
-context("  read data from singel file")
+context("  read data from single file")
 
 # TaroData.readDataFromDir
 test_that("  read data from singel file", {
-  data <- TaroData.readDataFromDir(paste(test.data_path, "/2013-01-02.csv", sep='/'), pattern=".csv")
+  data <- TaroData.readDataFromDir(paste(test.data_path, "2013/1/02.csv", sep='/'), pattern=".csv")
   # should return NULL
-  expect_equivalent(nrow(data), 3) 
+  expect_equivalent(nrow(data), 23) 
   
   first = head(data, n=1)
-  expect_equivalent(first[,3], 7842361474778)
+  expect_equivalent(first[,1], 8762650364684)
 })
 
 
@@ -36,8 +36,17 @@ context("  read data from multiples files")
 test_that("  read data from directory that doesn't have the files", {
   data <- TaroData.readDataFromDir(test.data_path, pattern=".csv")
   # should return NULL
-  expect_equivalent(nrow(data), 5) 
+  expect_equivalent(nrow(data), 2131) 
   
   first = head(data, n=1)
-  expect_equivalent(first[,3], 9992194378756)
+  expect_equivalent(first[,1], 9992194378756)
+})
+
+test.data_path <- paste(test.data_path , "/2013" ,sep="")
+
+context("  list directories from a directory")
+
+test_that("  list directories from a directory", {
+  dirs <- TaroData.listDirs(test.data_path)
+  expect_equivalent(length(dirs), 58)
 })
